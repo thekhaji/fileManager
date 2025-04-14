ca82d07a6d3c0cd108b378dcd6850d4adc59d1ea
thekhaji <khojiakbar_kodirov@mail.ru>
2025-04-14 14:04:24 +0900

    BRR: Initial setup

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express, { urlencoded } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Enterance
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+// View
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
+
+
+// Routes
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,6 @@
+
+import app from "./app.js";
+
+app.listen(3000, ()=>{
+    console.log("The server is running on PORT 3000 successfully!!!");
+});
