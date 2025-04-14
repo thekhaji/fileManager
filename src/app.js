71c45dcfc7f466d70a277bf8e99f69c7220d9156
thekhaji <khojiakbar_kodirov@mail.ru>
2025-04-14 16:03:04 +0900

    feat: developing front-end page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import express, { urlencoded } from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -19,6 +20,11 @@ app.set('view engine', 'ejs');
 // Routes
 app.get("/", (req, res)=>{
     res.render("index");
+});
+app.get("/download", (req, res)=>{
+    
+    console.log(req.query.link);
+    res.send("Done");
 });
 
 export default app;
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,41 @@
 console.log("frontend js is working!");
+const form = document.getElementById("myForm");
+const inputGroup = document.getElementsByClassName("inputGroup");
+const submitButton = form.querySelector('button[type="submit"]');
+let inputCounter = 1;
+
+function inputGroupTemplate(){
+    inputCounter+=1;
+    return `<div class="inputGroup">
+                <input type="text" placeholder="enter the link" name="link">
+                <button type="button" class="delete">x</button>
+                <button type="button" class="add">+</button>
+            </div>`;
+
+}
+
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("delete")){
+        console.log("deleting");
+        if(inputCounter>1){
+            e.target.parentElement.remove();
+            inputCounter-=1;
+        }
+        else
+            alert("The input can't be deleted, you can edit it");
+    }
+
+    else if(e.target.classList.contains("add")){
+        console.log("adding");
+        submitButton.insertAdjacentHTML("beforebegin", inputGroupTemplate());
+    }
+});
+
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault();
+//     const links = Array.from(
+//         inputGroup.querySelectorAll('input[name="link"]').map(input => input.value)
+//     );
+//     console.log("links:", links);
+    
+// });
